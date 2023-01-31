932303eed0296dc30f8a61440127d6b6138d5de9
nhicksn <nhicksn@umich.edu>
2023-01-31 12:41:04 -0500

    a

---
+++ p2a-heap.cpp
@@ -0,0 +1,6 @@
+// Project Identifier: AD48FB4835AF347EB0CA8009E24C3B13F8519882
+#include "p2a-heap.h"
+
+int main() {
+    std::ios::sync_with_stdio(false);
+}
---
+++ p2a-heap.h
@@ -0,0 +1,52 @@
+// Project Identifier: AD48FB4835AF347EB0CA8009E24C3B13F8519882
+#include <getopt.h>
+#include <iostream>
+
+
+class heap {
+private:
+    bool modeVerb = false;
+    bool modeMed = false;
+    bool modeGen = false;
+    bool modeWatch = false;
+
+public:
+    void getMode(const int &argc, char *argv[]) {
+        opterr = false;
+        int choice;
+        int index = 0;
+        option long_options[] = {
+            { "verbose",      no_argument,       nullptr, 'v' },
+            { "median",       no_argument,       nullptr, 'm' },
+            { "general-eval", no_argument,       nullptr, 'g' },
+            { "watcher",      no_argument,       nullptr, 'w' },
+            { nullptr,        0,                 nullptr, '\0'},
+        }; // long_options
+
+        while((choice = getopt_long(argc, argv, "vmgw", long_options, &index)) != -1) {
+            switch(choice) {
+                case 'v': {
+                    modeVerb = true;
+                } // case 'v'
+                
+                case 'm' : {
+                    modeMed = true;
+                } // case 'm'
+
+                case 'g': {
+                    modeGen = true;
+                } // case 'g'
+                
+                case 'w': {
+                    modeWatch = true;
+                } // case 'w'
+
+                default: {
+                    std::cerr << "Unknown command line option\n";
+                    exit(1);
+                } // default case
+            } // switch choice
+        } // while choice
+    } // getMode
+    
+};
