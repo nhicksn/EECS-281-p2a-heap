db116a6e07de6ea2bf98c1477655104b17a65137
nhicksn <nhicksn@umich.edu>
2023-02-02 12:12:16 -0500

    a

--- p2a-heap.cpp
+++ p2a-heap.cpp
@@ -1,6 +1,8 @@
 // Project Identifier: AD48FB4835AF347EB0CA8009E24C3B13F8519882
 #include "p2a-heap.h"
 
-int main() {
+int main(int argc, char *argv[]) {
     std::ios::sync_with_stdio(false);
+    heap h1;
+    h1.getMode(argc, argv);
 }
